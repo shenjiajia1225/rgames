b61060162a6bdbfbda7f6899c6c43b8a1910025d
shenjiajia1225@163.com <shenjiajia1225@163.com>
2021-01-11 14:03:23 +0800

    1. ..

--- gameserver/game1/move_obj.go
+++ gameserver/game1/move_obj.go
@@ -12,21 +12,32 @@ func init() {
 type MoveObj struct {
 	Pos        *pb.Vec3
 	Dir        *pb.Vec3
+	MvType     pb.MoveType
 	Speed      int32
 	LastUpTime int64
+	Targets    []*pb.Vec3
 }
 
 type MoveImpl interface {
-	Start(id int64, speed int32, dir *pb.Vec3)
+	Start(id int64, speed int32, mtype pb.MoveType, vecs []*pb.Vec3)
 	Stop(id int64)
 	TurnUp(id int64, speed int32, dir *pb.Vec3)
 	Update(now int64)
 }
 
-func (mo *MoveObj) Start(id int64, speed int32, dir *pb.Vec3) {
+func (mo *MoveObj) Start(id int64, speed int32, mtype pb.MoveType, vecs []*pb.Vec3) {
+	if vecs == nil || len(vecs) <= 0 || mtype == pb.MoveType_Stop {
+		return
+	}
+	mo.MvType = mtype
 	mo.Speed = speed
-	mo.Dir = dir
 	mo.LastUpTime = 0
+	if mtype == pb.MoveType_Direction {
+		mo.Dir = vecs[0]
+		mo.Targets = nil
+	} else if mtype == pb.MoveType_Direction {
+		mo.Targets = vecs
+	}
 	MoveMgrInstance().Add(id, mo)
 	utils.TLog.Debug(fmt.Sprintf("move start [%v] pos[%v,%v,%v] dir[%v,%v,%v] speed[%v]",
 		id, mo.Pos.X, mo.Pos.Y, mo.Pos.Z, mo.Dir.X, mo.Dir.Y, mo.Dir.Z, speed))
